e208580cff45d1bd41c119bcaa1308ecb68881b8
NikitaTerlyahin <mcmihawk@gmail.com>
2022-09-12 20:41:00 +0700

    init

---
+++ task 2/script.js
@@ -0,0 +1,6 @@
+const windowOuterWidth = window.outerWidth;
+const windowOuterHeight = window.outerHeight;
+
+function alerting() {
+  alert('Размер экрана' + window.outerWidth + 'x' + window.outerHeight);
+};
